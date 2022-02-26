0d4263d9cf2799a28ee0803cb08835dc3d5b5e57
ЧакНорисс <64031009+Ility-47@users.noreply.github.com>
2022-02-26 16:05:34 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,13 @@
-let male = document.getElementById("male");
-let female = document.getElementById("female");
+function next (){
+const  page1 =  document.getElementById('page__1');
+const  page2 =  document.getElementById('page__2');
+page1.style.display = "none";
+page2.style.display = "block";
+}
 
-male.addEventListener("click", function() {
-  this.classList.add("active");
-});
-
-female.addEventListener("click", function() {
-    this.classList.add("active");
-  });
-
-
-//let next = document.getElementById('next');
-//let next2 = document.getElementById('next2');
-//let page = document.querySelectorAll('page__1');
-//let page2 = document.querySelector('page__2');
-//let page3 = document.querySelector('page__3');
-//
-//next.addEventListener("click", function(){
-//    page.style.display = 'none';
-//    page2.style.display = 'block';
-//})
-
-
-//function next (){
-//    document.getElementById('page__1').style.display = "none";
-//    document.getElementById('page__2').style.display = "block";
-//}
+function nextnext (){
+    const  page2 =  document.getElementById('page__2');
+    const  page3 =  document.getElementById('page__3');
+    page2.style.display = "none";
+    page3.style.display = "flex";
+}
